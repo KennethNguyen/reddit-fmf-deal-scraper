5f4aaadc7ec0c85960285306db589aa2d5194cc6
Kenneth Nguyen <n.kennethcs@gmail.com>
2020-01-08 22:38:11 -0800

    Send the top 5 hot posts titles to Reddit user's inbox through private message

--- fmf-bot.py
+++ fmf-bot.py
@@ -1,6 +1,19 @@
+import praw
 from secret import reddit
 
-# get 5 hot posts from the frugalmalefashion subreddit
-hot_posts = reddit.subreddit('frugalmalefashion').hot(limit=5)
-for post in hot_posts:
-    print(post.title)
+def main():
+    # get 5 hot posts from the frugalmalefashion subreddit
+    # can be top('hour/day/week/month/year', limit=x) or new(limit=x), etc.
+    hot_posts = reddit.subreddit('frugalmalefashion').hot(limit=5)
+    
+    # each post title is concatenated to user_message string and used as PM to send to user
+    user_message = ''
+    for post in hot_posts:
+        print(post.title)
+        user_message += post.title + '  \n' # double space followed by new line allows PM format to have line breaks for each post title
+
+    # messages user 
+    reddit.redditor('SaltGrizzly').message('Hot Post Roundup', user_message)
+
+if __name__ == "__main__":
+    main()
